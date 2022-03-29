929293b2e0bdbec92e9dad0f3b648fd8639a9ab2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-03-29 10:23:21 -0400

    Number Spiral: Accepted

--- Introductory Problems/number-spiral.cpp
+++ Introductory Problems/number-spiral.cpp
@@ -7,11 +7,12 @@ int main() {
   while (t--) {
     int y, x;
     cin >> y >> x;
-    long long n = 1;
-    int d = max(y, x);
-    for (int r = 1; r < d; r++) {
-      n += 2 * r;
-    }
+    long long d = max(y, x);
+    // 1 + 2 + 4 + 6 + 8 + ... + d - 1 =
+    // 1 + 2 * (1 + 2 + 3 + 4 + ... + d - 1) =
+    // 1 + 2 * (1 + d - 1) * (d - 1) / 2 =
+    // 1 + d * (d - 1)
+    long long n = 1 + d * (d - 1);
     if (d % 2 == 0) {
       if (x < d) {
         n += d - x;
