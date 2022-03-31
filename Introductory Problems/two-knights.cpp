16c42640a50d573efd7469641157911a6df60548
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-03-30 21:43:39 -0400

    Update two-knights.cpp

--- Introductory Problems/two-knights.cpp
+++ Introductory Problems/two-knights.cpp
@@ -16,21 +16,21 @@ long long solve(long long n) {
   if (n == 3) {
     return 28;
   }
-  // There are 3 pieces that attack 2 points.
+  // There are 3 pieces that attack 2 cells in the last row or column.
   long long s1 = 3 * (n * n - 1 - 2);
-  // There are 4 pieces that attack 3 points.
+  // There are 4 pieces that attack 3 cells.
   long long s2 = 4 * (n * n - 1 - 3);
-  // There are (2 * n - 1 - 3 - 4) pieces that attack 4 points.
+  // The rest of the pieces in the last row or column, i.e. (2 * n - 1 - 3 - 4) attack 4 cells.
   long long s3 = (2 * n - 1 - 3 - 4) * (n * n - 1 - 4);
   // There are (a + b) / 2 combinations where both knights are 
   // on the last row or column.
   // a = 4 * (2 * n - 1 - 1)
-  // There are 4 positions where both knights are 
+  // There are 4 cells where both knights are 
   // on the last row or column and attack each other, thus there are only
-  // (2 * n - 1 - 1) positions available for the next knight.
+  // (2 * n - 1 - 1) cells available for the next knight.
   // b = (2 * n - 1 - 4) * (2 * n - 1 - 2)
-  // For all other positions in the last row or column
-  // (except the above 4) there are (2 * n - 1 - 2) positions 
+  // For all other cells in the last row or column
+  // (except the above 4) there are (2 * n - 1 - 2) cells 
   // available for the next knight.
   // And since we counted the order of knights, we have to divide 
   // the sum by 2.
