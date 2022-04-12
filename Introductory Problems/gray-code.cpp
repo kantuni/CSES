a8b8ccb72b7e90fb62feac15f85aef40008b47c1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-11 22:43:07 -0400

    Update gray-code.cpp

--- Introductory Problems/gray-code.cpp
+++ Introductory Problems/gray-code.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 map<string, int> color;
 
-void dfs(string& s) {
-  cout << s << endl;
+void backtrack(string& s) {
+  cout << s << "\n";
   color[s] = 1;
   for (int i = 0; i < (int) s.size(); i++) {
     // flip the bit
     s[i] = (s[i] == '0') ? '1': '0';
     // check that we haven't been in that state before
     if (color.count(s) == 0) {
-      dfs(s);
+      backtrack(s);
       return;
     }
     // if we have, restore the previous value
@@ -23,6 +23,6 @@ int main() {
   int n;
   cin >> n;
   string root = string(n, '0');
-  dfs(root);
+  backtrack(root);
   return 0;
 }
