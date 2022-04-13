27b15081d3efb449505ddbdbd113afd3184c2c12
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-13 14:45:01 -0400

    Concert Tickets: TLE

---
+++ Sorting and Searching/concert-tickets.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  multiset<int> h;
+  for (int i = 0; i < n; i++) {
+    int p;
+    cin >> p;
+    h.insert(p);
+  }
+  vector<int> t(m);
+  for (int i = 0; i < m; i++) {
+    cin >> t[i];
+  }
+  for (int i = 0; i < m; i++) {
+    auto nxt = upper_bound(h.begin(), h.end(), t[i]);
+    if (nxt == h.begin()) {
+      cout << -1 << "\n";
+      continue;
+    }
+    auto it = prev(nxt);
+    cout << *it << "\n";
+    h.erase(it);
+  }
+  return 0;
+}
