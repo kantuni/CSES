b97c1814e2099ee8d7a962cb2911e2caddaac76c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-13 20:43:44 -0400

    Restaurant Customers: Accepted

---
+++ Sorting and Searching/restaurant-customers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> events;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    cin >> a >> b;
+    events.push_back({a, 1});
+    events.push_back({b, -1});
+  }
+  sort(events.begin(), events.end());
+  int ans = 0, cur = 0;
+  for (auto [time, type]: events) {
+    cur += type;
+    ans = max(ans, cur);
+  }
+  cout << ans << "\n";
+  return 0;
+}
