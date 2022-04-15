c6877618fa84d51ad719c6f7b3fff5fee4d459c5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-15 13:32:25 -0400

    Missing Coin Sum: TLE (again)

--- Sorting and Searching/missing-coin-sum.cpp
+++ Sorting and Searching/missing-coin-sum.cpp
@@ -1,36 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<long long, int> memo;
+vector<long long> a;
 
-bool find(long long x) {
+bool find(long long x, vector<long long>::iterator end) {
   if (x == 0) {
     return true;
   }
-  bool ok = false;
-  for (auto [num, cnt]: memo) {
-    if (num > x) {
-      break;
-    }
-    if (memo[num] > 0) {
-      memo[num]--;
-      ok = ok or find(x - num);
-      memo[num]++;
-    }
+  auto nxt = upper_bound(a.begin(), end, x);
+  if (nxt == a.begin()) {
+    return false;
   }
-  return ok;
+  auto it = prev(nxt);
+  return find(x - *it, it);
 }
 
 int main() {
   int n;
   cin >> n;
-  vector<long long> a(n);
+  a.assign(n, 0);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
-    memo[a[i]]++;
   }
+  sort(a.begin(), a.end());
   long long ans = 1;
-  while (find(ans)) {
+  while (find(ans, a.end())) {
     ans++;
   }
   cout << ans << "\n";
