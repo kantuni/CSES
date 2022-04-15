b3e53de94624896daf4f38920d2624ec8bc224f3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-15 18:30:14 -0400

    Missing Coin Sum: Accepted

--- Sorting and Searching/missing-coin-sum.cpp
+++ Sorting and Searching/missing-coin-sum.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long long> a;
-
-bool find(long long x, vector<long long>::iterator end) {
-  if (x == 0) {
-    return true;
-  }
-  auto nxt = upper_bound(a.begin(), end, x);
-  if (nxt == a.begin()) {
-    return false;
-  }
-  auto it = prev(nxt);
-  return find(x - *it, it);
-}
-
 int main() {
   int n;
   cin >> n;
-  a.assign(n, 0);
+  vector<long long> a(n);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
   sort(a.begin(), a.end());
-  long long ans = 1;
-  while (find(ans, a.end())) {
-    ans++;
+  long long ans = 0;
+  int i = 0;
+  while (i < n and ans + 1 >= a[i]) {
+    ans += a[i];
+    i++;
   }
-  cout << ans << "\n";
+  cout << ans + 1 << "\n";
   return 0;
 }
