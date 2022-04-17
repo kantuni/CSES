248d607832410a1726fb03a7824477c7502a113d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-16 23:33:48 -0400

    Collecting Numbers II: Accepted

--- Sorting and Searching/collecting-numbers-ii.cpp
+++ Sorting and Searching/collecting-numbers-ii.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main() {
   int n, m;
   cin >> n >> m;
-  vector<int> a(n), pos(n);
+  vector<int> a(n);
+  unordered_map<int, int> pos;
   for (int i = 0; i < n; i++) {
     cin >> a[i];
-    pos[a[i] - 1] = i;
+    pos[a[i]] = i;
   }
   int ans = 0;
-  for (int i = 0; i < n - 1; i++) {
-    if (pos[i + 1] < pos[i]) {
+  for (int i = 1; i < n; i++) {
+    if (pos[i] > pos[i + 1]) {
       ans++;
     }
   }
@@ -19,44 +20,37 @@ int main() {
     int i, j;
     cin >> i >> j;
     i--, j--;
-    // (i - 1, i) -> (i - 1, j)
-    if (i - 1 >= 0) {
-      if (pos[i - 1] > pos[i] and pos[i - 1] < pos[j]) {
-        ans--;
-      }
-      if (pos[i - 1] < pos[i] and pos[i - 1] > pos[j]) {
-        ans++;
+    if (i > j) {
+      swap(i, j);
+    }
+    set<pair<int, int>> s;
+    if (a[i] != 1) {
+      s.insert({a[i] - 1, a[i]});
+    }
+    if (a[i] != n) {
+      s.insert({a[i], a[i] + 1});
+    }
+    if (a[j] != 1) {
+      s.insert({a[j] - 1, a[j]});
+    }
+    if (a[j] != n) {
+      s.insert({a[j], a[j] + 1});
+    }
+    int d1 = 0;
+    for (auto [x, y]: s) {
+      if (pos[x] > pos[y]) {
+        d1++;
       }
     }
-    // (i, i + 1) -> (j, i + 1)
-    if (i + 1 < n) {
-      if (pos[i] > pos[i + 1] and pos[j] < pos[i + 1]) {
-        ans--;
-      }
-      if (pos[i] < pos[i + 1] and pos[j] > pos[i + 1]) {
-        ans++;
-      }
-    }
-    // (j - 1, j) -> (j - 1, i)
-    if (j - 1 >= 0) {
-      if (pos[j - 1] > pos[j] and pos[j - 1] < pos[i]) {
-        ans--;
-      }
-      if (pos[j - 1] < pos[j] and pos[j - 1] > pos[i]) {
-        ans++;
-      }
-    }
-    // (j, j + 1) -> (i, j + 1)
-    if (j + 1 < n) {
-      if (pos[j] > pos[j + 1] and pos[i] < pos[j + 1]) {
-        ans--;
-      }
-      if (pos[j] < pos[j + 1] and pos[i] > pos[j + 1]) {
-        ans++;
-      }
-    }
-    swap(pos[a[i] - 1], pos[a[j] - 1]);
+    swap(pos[a[i]], pos[a[j]]);
     swap(a[i], a[j]);
+    int d2 = 0;
+    for (auto [x, y]: s) {
+      if (pos[x] > pos[y]) {
+        d2++;
+      }
+    }
+    ans += d2 - d1;
     cout << ans + 1 << "\n";
   }
   return 0;
