676ba2f4704c981247bd8a30d7086940e5f1e449
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-16 23:42:45 -0400

    Refactor

--- Sorting and Searching/collecting-numbers-ii.cpp
+++ Sorting and Searching/collecting-numbers-ii.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main() {
   int n, m;
   cin >> n >> m;
-  vector<int> a(n);
-  unordered_map<int, int> pos;
+  vector<int> a(n), pos(n);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
-    pos[a[i]] = i;
+    pos[a[i] - 1] = i;
   }
   int ans = 0;
-  for (int i = 1; i < n; i++) {
+  for (int i = 0; i < n - 1; i++) {
     if (pos[i] > pos[i + 1]) {
       ans++;
     }
@@ -20,30 +19,30 @@ int main() {
     int i, j;
     cin >> i >> j;
     i--, j--;
-    set<pair<int, int>> s;
+    set<pair<int, int>> changed;
     if (a[i] != 1) {
-      s.insert({a[i] - 1, a[i]});
+      changed.insert({a[i] - 1, a[i]});
     }
     if (a[i] != n) {
-      s.insert({a[i], a[i] + 1});
+      changed.insert({a[i], a[i] + 1});
     }
     if (a[j] != 1) {
-      s.insert({a[j] - 1, a[j]});
+      changed.insert({a[j] - 1, a[j]});
     }
     if (a[j] != n) {
-      s.insert({a[j], a[j] + 1});
+      changed.insert({a[j], a[j] + 1});
     }
     int d1 = 0;
-    for (auto [x, y]: s) {
-      if (pos[x] > pos[y]) {
+    for (auto [x, y]: changed) {
+      if (pos[x - 1] > pos[y - 1]) {
         d1++;
       }
     }
-    swap(pos[a[i]], pos[a[j]]);
+    swap(pos[a[i] - 1], pos[a[j] - 1]);
     swap(a[i], a[j]);
     int d2 = 0;
-    for (auto [x, y]: s) {
-      if (pos[x] > pos[y]) {
+    for (auto [x, y]: changed) {
+      if (pos[x - 1] > pos[y - 1]) {
         d2++;
       }
     }
