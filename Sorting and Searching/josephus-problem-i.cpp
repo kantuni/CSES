b7a2cb053e4c72585ab4f64e57f72c3ca94b6405
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-18 13:02:16 -0400

    Josephus Problem I: Accepted

---
+++ Sorting and Searching/josephus-problem-i.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  set<int> s;
+  for (int i = 1; i <= n; i++) {
+    s.insert(i);
+  }
+  auto it = s.begin();
+  int k = 2;
+  while (s.size() > 0) {
+    for (int i = 0; i < k - 1; i++) {
+      if (next(it) == s.end()) {
+        it = s.begin();
+      } else {
+        it++;
+      }
+    }
+    cout << *it << " ";
+    auto rmit = it;
+    if (next(it) == s.end()) {
+      it = s.begin();
+    } else {
+      it++;
+    }
+    s.erase(rmit);
+  }
+  cout << "\n";
+  return 0;
+}
