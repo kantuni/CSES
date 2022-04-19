7a233f78281ab4f7598ee05844951a0834133bc2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-19 00:59:33 -0400

    Josephus Problem II: Accepted

--- Sorting and Searching/josephus-problem-ii.cpp
+++ Sorting and Searching/josephus-problem-ii.cpp
@@ -1,30 +1,34 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update> indexed_set;
 
 int main() {
   int n, k;
   cin >> n >> k;
-  set<int> s;
+  indexed_set s;
   for (int i = 1; i <= n; i++) {
     s.insert(i);
   }
   auto it = s.begin();
+  int offset = 0;
   while (s.size() > 0) {
-    for (int i = 0; i < (int) k % s.size(); i++) {
-      if (next(it) == s.end()) {
-        it = s.begin();
-      } else {
-        it++;
-      }
-    }
+    offset = (offset + k) % s.size();
+    it = s.find_by_order(offset);
     cout << *it << " ";
-    auto rmit = it;
+    auto tmp = it;
     if (next(it) == s.end()) {
       it = s.begin();
+      offset = 0;
     } else {
       it++;
+      offset++;
     }
-    s.erase(rmit);
+    offset = (offset - 1 + s.size()) % s.size();
+    s.erase(tmp);
   }
   cout << "\n";
   return 0;
