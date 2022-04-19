ea53326d222f945aaef400ef0c797ea8ce1b9da1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-19 01:14:06 -0400

    Refactor

--- Sorting and Searching/josephus-problem-ii.cpp
+++ Sorting and Searching/josephus-problem-ii.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-             tree_order_statistics_node_update> indexed_set;
+template <typename T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, 
+                         tree_order_statistics_node_update>;
 
 int main() {
   int n, k;
   cin >> n >> k;
-  indexed_set s;
+  indexed_set<int> s;
   for (int i = 1; i <= n; i++) {
     s.insert(i);
   }
