ee0768a102c1e80cb18bf00aba85e301c0ebda08
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-20 13:17:31 -0400

    Update josephus-problem-ii-v2.cpp

--- Sorting and Searching/josephus-problem-ii-v2.cpp
+++ Sorting and Searching/josephus-problem-ii-v2.cpp
@@ -4,13 +4,13 @@ using namespace std;
 using namespace __gnu_pbds;
 
 template <typename T>
-using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, 
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, 
                          tree_order_statistics_node_update>;
 
 int main() {
   int n, k;
   cin >> n >> k;
-  indexed_set<int> s;
+  ordered_set<int> s;
   for (int i = 1; i <= n; i++) {
     s.insert(i);
   }
