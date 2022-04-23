18b5a75f44bfd63a2c1327532a8129af2479c821
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-23 12:33:21 -0400

    Room Allocation: Accepted

---
+++ Sorting and Searching/room-allocation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+using max_heap = priority_queue<T, vector<T>, greater<T>>;
+
+int main() {
+  int n;
+  cin >> n;
+  // left, right, index
+  vector<tuple<int, int, int>> events;
+  for (int i = 0; i < n; i++) {
+    int l, r;
+    cin >> l >> r;
+    events.push_back({l, r, i});
+  }
+  sort(events.begin(), events.end());
+  // right, key
+  max_heap<pair<int, int>> pq;
+  pq.push({0, 1});
+  vector<int> ans(n);
+  int nxt = 2;
+  for (auto [l, r, i]: events) {
+    auto [mr, k] = pq.top();
+    int key = -1;
+    if (l > mr) {
+      key = k;
+      pq.pop();
+    } else {
+      key = nxt;
+      nxt++;
+    }
+    ans[i] = key;
+    pq.push({r, key});
+  }
+  cout << nxt - 1 << "\n";
+  for (auto key: ans) {
+    cout << key << " ";
+  }
+  cout << "\n";
+  return 0;
+}
