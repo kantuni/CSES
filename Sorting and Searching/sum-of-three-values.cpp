7b49639c1fe3e4cb40004215cc717573d114d002
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-26 01:16:27 -0400

    Sum of Three Values: Accepted

---
+++ Sorting and Searching/sum-of-three-values.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, x;
+  cin >> n >> x;
+  vector<pair<int, int>> a(n);
+  for (int i = 0; i < n; i++) {
+    int num;
+    cin >> num;
+    a[i] = {num, i};
+  }
+  sort(a.begin(), a.end());
+  bool found = false;
+  for (int i = 0; i < n - 2; i++) {
+    for (int j = i + 1; j < n - 1; j++) {
+      int target = x - a[i].first - a[j].first;
+      pair<int, int> p = {target, INT_MIN};
+      auto lower = lower_bound(a.begin() + j + 1, a.end(), p);
+      if (lower != a.end() and (*lower).first == target) {
+        int k = lower - a.begin();
+        cout << a[i].second + 1 << " ";
+        cout << a[j].second + 1 << " ";
+        cout << a[k].second + 1 << "\n";
+        found = true;
+        break;
+      }
+    }
+  }
+  if (!found) {
+    cout << "IMPOSSIBLE" << "\n";
+  }
+  return 0;
+}
