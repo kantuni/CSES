f333985bb76cf96c142dfa5e4095f3891ee2bb7a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-27 13:12:48 -0400

    Another TLE

--- Sorting and Searching/sum-of-four-values.cpp
+++ Sorting and Searching/sum-of-four-values.cpp
@@ -2,30 +2,45 @@
 using namespace std;
  
 int main() {
-  int n, x;
+  int n;
+  long long x;
   cin >> n >> x;
-  vector<pair<int, int>> a(n);
+  vector<pair<long long, int>> a(n);
+  long long sum = 0;
   for (int i = 0; i < n; i++) {
     int num;
     cin >> num;
     a[i] = {num, i};
+    sum += num;
+  }
+  if (sum < x) {
+    cout << "IMPOSSIBLE" << "\n";
+    return 0;
   }
   sort(a.begin(), a.end());
+  a.push_back({0, -1});
+  map<long long, int> nxt;
+  for (int i = 0; i < n; i++) {
+    if (a[i].first != a[i + 1].first) {
+      nxt[a[i].first] = i + 1;
+    }
+  }
+  a.pop_back();
   bool found = false;
   for (int i = 0; !found and i < n - 3; i++) {
     if (i > 0 and a[i].first == a[i - 1].first) {
-      continue;
+      i = nxt[a[i].first];
     }
     for (int j = i + 1; !found and j < n - 2; j++) {
       if (j > i + 1 and a[j].first == a[j - 1].first) {
-        continue;
+        j = nxt[a[j].first];
       }
       for (int k = j + 1; !found and k < n - 1; k++) {
         if (k > j + 1 and a[k].first == a[k - 1].first) {
-          continue;
+          k = nxt[a[k].first];
         }
-        int target = x - a[i].first - a[j].first - a[k].first;
-        pair<int, int> p = {target, INT_MIN};
+        long long target = x - a[i].first - a[j].first - a[k].first;
+        pair<long long, int> p = {target, INT_MIN};
         auto lower = lower_bound(a.begin() + k + 1, a.end(), p);
         if (lower != a.end() and (*lower).first == target) {
           int l = lower - a.begin();
