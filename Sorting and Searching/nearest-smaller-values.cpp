a6f0d925793f6e97726d3c851992f64288c69fd1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-04-28 15:00:12 -0400

    Nearest Smaller Values: TLE

---
+++ Sorting and Searching/nearest-smaller-values.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> a;
+
+// range min query
+int rmq(int l, int r) {
+  if (l < 0 or r > n - 1) {
+    return 0;
+  }
+  if (l == r) {
+    return a[l];
+  }
+  int m = (l + r) / 2;
+  return min(rmq(l, m), rmq(m + 1, r));
+}
+
+int main() {
+  cin >> n;
+  a.assign(n, 0);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  for (int i = 0; i < n; i++) {
+    int mn = a[i];
+    int r = i, l = r - 1;
+    for (int p = 0; true; p++) {
+      l = max(0, r - (1 << p));
+      int res = rmq(l, r);
+      if (res < mn) {
+        mn = res;
+        break;
+      }
+      if (l == 0) {
+        break;
+      }
+    }
+    // no value before a[i] is less than a[i]
+    if (mn == a[i]) {
+      cout << 0 << " ";
+      continue;
+    }
+    while (l < r) {
+      int m = (l + r) / 2;
+      if (rmq(l, m) > mn) {
+        l = m + 1;
+      } else {
+        r = m - 1;
+      }
+    }
+    cout << l + 1 << " ";
+  }
+  cout << "\n";
+  return 0;
+}
