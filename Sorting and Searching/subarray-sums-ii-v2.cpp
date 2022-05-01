8dc49d5d9a06e64331f44e9fb3a04c50556184a9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-05-01 18:08:34 -0400

    Refactor

--- Sorting and Searching/subarray-sums-ii-v2.cpp
+++ Sorting and Searching/subarray-sums-ii-v2.cpp
@@ -5,17 +5,18 @@ int main() {
   int n;
   long long x;
   cin >> n >> x;
+  // Adding 0 because the whole interval can be selected.
   map<long long, long long> memo = {{0, 1}};
-  long long sum = 0, ans = 0;
+  long long ps = 0, ans = 0;
   for (int i = 0; i < n; i++) {
     int num;
     cin >> num;
-    sum += num;
-    ans += memo[sum - x];
+    ps += num;
+    ans += memo[ps - x];
     // Updating the count should come before calculating
     // the answer. Otherwise, x = 0 scenarios will
     // produce an incorrect answer.
-    memo[sum]++;
+    memo[ps]++;
   }
   cout << ans << "\n";
   return 0;
