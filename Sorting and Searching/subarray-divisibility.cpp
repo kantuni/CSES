f6069fd195c39f7e30fdcba8158ee56489961fb8
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-05-02 13:53:43 -0400

    Subarray Divisibility: Accepted

--- Sorting and Searching/subarray-divisibility.cpp
+++ Sorting and Searching/subarray-divisibility.cpp
@@ -7,20 +7,17 @@ int main() {
   map<long long, long long> memo = {{0, 1}};
   long long ps = 0, ans = 0;
   for (int i = 0; i < n; i++) {
-    int num;
+    long long num;
     cin >> num;
     ps += num;
-    long long mn = memo.begin()->first;
-    long long mnn = (mn / n) * n;
-    // The current prefix sum might be the largest,
-    // but we haven't pushed it into the map yet.
-    long long mx = max(ps, prev(memo.end())->first);
-    long long mxn = (mx / n) * n;
-    for (long long k = mnn; k <= mxn; k += n) {
-      ans += memo[ps - k];
+    long long rem = ps % n;
+    if (rem < 0) {
+      rem += n;
     }
-    memo[ps]++;
+    ans += memo[rem];
+    memo[rem]++;
   }
-  cout << ans << "\n";
+  cout << ans << "\n"; 
   return 0;
 }
+
